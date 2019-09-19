934de28c919590d7b3fce92e39d35f6aa55e8466
Arnaud Ardans II and fcw1987@mac.com <arnaud@>
2019-09-18 20:28:00 -0700

    completes quiz calculator and character selection with error reporting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,45 @@
 // Gobal Variables
-
+var quizCalc = function(number1, number2, number3) {
+  return ((number1 + number2 + number3) % 4)
+};
 
 // Start of javascript
 $(document).ready(function() {
   $("form#characterQuiz").submit(function(event) {
-    var number1 = parseInt($("#age")).val();
-    var number1 = parseInt($("#favoriteColor").val());
-    var number2 = parseInt($("#favoriteFood").val());
-
-    console.log(number1);
-    console.log(number2);
-    console.log(number3);
-
-
     event.preventDefault();
+    var yourName = $("#firstName").val();
+    var number1 = parseInt($("#age").val());
+    var number2 = parseInt($("#favoriteColor").val());
+    // var number2 = parseInt($("#favoriteFood").val());
+    var number3 = $("input:radio[name=favoriteFood]:checked").val();
+
+    var result = quizCalc(number1, number2, number3);
+
+    console.log(result)
+    if ( result === 0 ) {
+      $("#output h2 #name").text(yourName);
+      $("#output h2 #seinfeldCharacter").text("Elaine");
+    } else if (result === 1) {
+      $("#output h2 #name").text(yourName);
+      $("#output h2 #seinfeldCharacter").text("Jerry");
+    } else if (result === 2) {
+      $("#output h2 #name").text(yourName);
+      $("#output h2 #seinfeldCharacter").text("Kramer");
+    } else if (result === 3) {
+      $("#output h2 #name").text(yourName);
+      $("#output h2 #seinfeldCharacter").text("Newman");
+    } else {
+      console.log("There was an error")
+    }
+
+
+    // Debugging
+    // console.log(number1);
+    // console.log(number2);
+    // console.log(number3);
+
+
+
     // var operator = $("input:radio[name=operator]:checked").val();
     // var result;
     // if (operator === "add") {
